d19573f5185e79b330f112a15eb84c9189a1f556
Ulik93 <uliksh@gmail.com>
2021-06-07 15:13:42 +0600

    baza

--- src/components/knowledgebase/Knowledgebase.jsx
+++ src/components/knowledgebase/Knowledgebase.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './knowledgebase.sass'
-const Knowledgebase = ()=>{
-    return(
-        <div className='knowledgebase'>
-          <h1>  База знаний</h1>
+import React from "react";
+import "./knowledgebase.css";
+import logo from "./images/logo.png";
+
+const Knowledgebase = () => {
+  return (
+    <div className="knowledgebase">
+      <div className="knowledgebase__wrapper">
+        <div className="knowledgebase__wrapper2">
+          {/* <div className="header__logo">
+            <img src={logo} alt="" />
+          </div> */}
+          <h1 className="knowledgebase__tittle"> База знаний</h1>
+          <div className="knowledgebase__input">
+            <input
+              type="search"
+              id="mySearch"
+              placeholder="Поиск по базе знаний"
+            />
+          </div>
         </div>
-    )
-}
- export default Knowledgebase
+        <div className="knowledgebase__container">
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Knowledgebase;
